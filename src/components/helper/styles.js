d0029d9cac0648ca9f4af67d425300818d0cf5e0
Vincent Chevalier <vchevalier@equesmedia.ca>
2020-09-17 10:56:01 -0400

    Hack error style

--- src/components/helper/styles.js
+++ src/components/helper/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   text: {
     flex: 1,
+    marginLeft: -11,
     fontSize: 12,
     lineHeight: 16,
     backgroundColor: 'transparent',
